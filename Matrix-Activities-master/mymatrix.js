3e1ecb4001b7dc8ca9d21913d06d051c224747a6
jesusromero19 <9jromero@stpatrick.org>
2017-01-20 13:56:17 -0600

    this is quite confusing

--- Matrix-Activities-master/mymatrix.js
+++ Matrix-Activities-master/mymatrix.js
@@ -7,11 +7,4 @@ let N = Matrix([[2,3],[3,5]]);
 console.log(M.prod(N));
 
 //Draft your code below this line to answer the questions.
-let A = [0,1,1,0,0,1]
-        [1,0,1,0,0,1]
-        [1,1,0,1,1,1]
-        [0,0,1,0,0,0]
-        [0,0,1,0,0,0]
-        [1,1,1,0,0,0]
 //You can 'comment out' theexample above by placing double slashes before them.
-console.log(A.prod(a));
